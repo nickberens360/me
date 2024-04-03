c3beed7a6970582d86215a532eeccd83b12c8478
Nick Berens <nickberens360@gmail.com>
2024-04-02 19:08:07 -0500

    creat reveal-text page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 import './currentYear.js';
 import './fetchCats.js';
 import './modal.js';
+
+async function getServerSideProps () {
+  const response = await fetch('https://api.github.com/users/nickberens360/repos');
+  const json = await response.json();
+  return json;
+}
+
+getServerSideProps().then(data => {
+  console.log(data);
+} );
