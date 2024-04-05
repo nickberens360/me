1b7b54633d58acc093cc44d196081e833203e9a4
Nick Berens <nickberens360@gmail.com>
2024-04-04 21:09:03 -0500

    remove js cruft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
 import './currentYear.js';
 import './fetchCats.js';
 import './modal.js';
-
-async function getServerSideProps () {
-  const response = await fetch('https://api.github.com/users/nickberens360/repos');
-  const json = await response.json();
-  return json;
-}
-
-getServerSideProps().then(data => {
-  console.log(data);
-} );
